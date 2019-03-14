52be7b87a247e91c2a5520d7a579c32740d521cf
ธิดารัตน์ จีบมัจฉา <s6002041510055@Macmini-213-02.local>
2019-03-14 16:40:02 +0700

    create App component

---
+++ components/App.js
@@ -0,0 +1,17 @@
+// import Library
+import React from 'react';
+import { View, Text  } from 'react-native';
+
+// write component
+class App extends React.Component {
+    render() {
+        return (
+                <View>
+                    <Text>Hello Word!!!</Text>
+                </View>
+        );
+    }
+}
+
+// extent
+export default App;
