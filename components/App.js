88ff66dbd10b930d18c0d9d9d54270e5e9843120
ธิดารัตน์ จีบมัจฉา <s6002041510055@Macmini-213-02.local>
2019-03-28 14:24:44 +0700

    Using Header component on App

--- components/App.js
+++ components/App.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import { View, Text  } from 'react-native';
 
+import Header from './Header'
 // write component
 class App extends React.Component {
     render() {
         return (
                 <View>
+                    <Header title="Albums" />
                     <Text style={{fontSize: 25, color: '#E11584', textAlign: "center"}}>Tidarat Jibmagcha</Text>
                 </View>
         );
